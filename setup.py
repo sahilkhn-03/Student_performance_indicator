4f03f293e7130e1f9c6368dc6eaf026684e09910
sahilkhn-03 <sahilkhankhk5881@gmail.com>
2026-02-25 15:07:53 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml-project",
+    version="0.0.1",
+    description="Machine Learning Project",
+    author="Sahil",
+    author_email="sahilkhk001@gmail.com",
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
